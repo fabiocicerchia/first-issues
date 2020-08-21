0fe2b74e0ae09b159a4ad25dcbc28a625cccb341
Fabio Cicerchia <info@fabiocicerchia.it>
2020-08-21 07:46:07 +0200

    generated static version

---
+++ precache-manifest.94736ec6821ac4b5f3add9b7b2881e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954d85c1df1036ab544f08f06e7b146d",
+    "url": "/first-issues/index.html"
+  },
+  {
+    "revision": "1c73c798248f1809e080",
+    "url": "/first-issues/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5aed48448a4968083a26",
+    "url": "/first-issues/static/js/2.f4e0bc8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/first-issues/static/js/2.f4e0bc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c73c798248f1809e080",
+    "url": "/first-issues/static/js/main.af84a7cb.chunk.js"
+  },
+  {
+    "revision": "aacfdf24b65e5c25bc87",
+    "url": "/first-issues/static/js/runtime-main.0a9fea3f.js"
+  }
+]);
